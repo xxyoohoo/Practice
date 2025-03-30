ddac58c07292289df954c905ed607a88b5026c9e
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-03-30 16:13:29 -0400

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,9 @@
+//https://open.kattis.com/submissions/17030384
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "Hello World!";    
+}
