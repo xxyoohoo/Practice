ce1c5ce0673a153df66cffff499af731fa361110
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-04-28 06:51:23 -0400

    Create N-sum.cpp

---
+++ N-sum.cpp
@@ -0,0 +1,10 @@
+//https://open.kattis.com/submissions/13834344
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y=0;
+    cin >> x;
+    while(cin >> x) y+=x;
+    cout << y;
+}
