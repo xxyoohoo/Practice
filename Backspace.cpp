d2ccfcd0242b28adf3414743f440604a3729db28
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-05-27 16:20:44 -0400

    Update and rename Backspace to Backspace.cpp

--- Backspace.cpp
+++ Backspace.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     string s;
     cin >> s;
     int lens, x=0;
@@ -11,10 +13,6 @@ int main()
         if(s[i]=='<') {x++; s[i]='.';}
         else if (x>0) {s[i]='.'; x--;}
     }
-    for(int i=0;i<lens;++i)
-    {
-        if(s[i]!='.')
-            cout << s[i];
-    }
+    for(int i=0;i<lens;++i) if(s[i]!='.') cout << s[i];
     return 0;
 }
