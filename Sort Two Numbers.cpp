c5a2556e6841499ee0deafda05844777fa1c576a
nitrogenberry <131041571+xxyoohoo@users.noreply.github.com>
2025-06-03 21:57:20 -0400

    Create Sort Two Numbers.cpp

---
+++ Sort Two Numbers.cpp
@@ -0,0 +1,9 @@
+//https://open.kattis.com/submissions/13834158
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin >> x >> y;
+    cout << min(x,y) << " " << max(x,y);
+}
