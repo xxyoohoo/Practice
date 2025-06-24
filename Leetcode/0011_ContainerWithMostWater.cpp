cbd09d51d37e42ab05c487dcf286ff588f67c88a
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-06-24 13:56:48 -0400

    Create 0011_ContainerWithMostWater.cpp

---
+++ Leetcode/0011_ContainerWithMostWater.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0, r=height.size()-1, ans=0;
+        while(l<r)
+        {
+            int curr=(r-l)*min(height[l],height[r]);
+            ans=max(ans,curr);
+            if(height[l]<height[r]) ++l;
+            else --r;
+        }
+        return ans;
+    }
+};
