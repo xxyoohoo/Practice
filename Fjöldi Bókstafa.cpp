9e19b71b7395f466b2c3bee1795c3e94659aa1cb
xxyoohoo <131041571+xxyoohoo@users.noreply.github.com>
2025-07-04 07:46:02 -0400

    Create Fjöldi Bókstafa.cpp

---
+++ Fjöldi Bókstafa.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin >> s;
+    cout << s.length();
+    return 0;
+}
